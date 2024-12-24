2a82753408d98404dbda5a714431e41ea7c66823
Muhammad-Unaid <139008731+Muhammad-Unaid@users.noreply.github.com>
2024-12-24 12:57:53 +0500

    Add files via upload

---
+++ first-class.py
@@ -0,0 +1,56 @@
+
+print("enter your name")
+name = input()
+print("my name is ", name)
+
+print("where you from")
+place = input()
+print("I'm from ", place)
+
+print("how old are you")
+age = input()
+print("I'm ", age + "year old")
+
+print("which cousre are you learning now")
+course_name = input()
+print("I'm learning" , course_name)
+
+
+
+
+
+
+# name = input("enter your name?\n")
+# name = name.strip()
+# name = name.upper()
+# print(f"my name is  {name}")
+
+
+# place = input("where you from\n")
+# place = place.strip()
+# place = place.lower()
+# print(f"I'm from {place}" )
+
+
+
+# num1 = int(input())
+# num2 = int(input())
+# print(num1+num2)
+
+# num1 = 5
+# num2 = 6
+# print(num1*num2)               
+
+
+def calculation(num3,num4):
+    ans = num3+num4
+    print(ans)
+    ans = num3-num4
+    print(ans)
+    ans = num3*num4
+    print(ans)
+    ans = num3/num4
+    print(ans)
+
+calculation(2,3)
+
---
+++ sec-class.py
@@ -0,0 +1,76 @@
+# def square():
+#     length = float(input("enter a value"))
+#     ans = length*length
+#     print(ans)
+
+# square()
+
+
+# def speed():
+#     d=int(input('Enter distance in km '))
+#     t=int(input('Enter time in minutes '))
+#     s=d/t
+#     print(f'Speed {s}km per minute')
+
+# speed()
+
+
+
+
+"""
+def calculate_speed(distance,time):
+    speed = distance / time
+    return speed 
+
+distance = float(input("enter distance in km :"))
+time = float(input("enter time in min :"))
+
+speed = calculate_speed(distance, time)
+print (f"your speed is {speed} m/s")
+"""
+
+
+
+
+# def tip_calculate(bill,persontage):
+#     tip = bill*persontage/100
+#     print(tip)
+
+# bill = float(input("enter your bill"))
+# persontage = float(input("enter your persontage"))
+
+# tip_calculate(bill,persontage)
+
+"""
+def tip_calculate(bill,persontage):
+    tip = bill*persontage/100
+    return tip
+
+bill = float(input("enter your bill"))
+persontage = float(input("enter your persontage"))
+
+tip = tip_calculate(bill,persontage)
+print(tip)
+"""
+"""
+def mileage(distance, petrol):
+    tra = distance / petrol
+    return tra
+
+distance = float(input("enter your distance"))
+petrol = float(input("enter your petrol "))
+
+tra =mileage(distance, petrol)
+print(tra*2)
+"""
+
+animal = input("enter your pet animal")
+
+if  animal == "dog":
+    print ("you have enter dog ")
+elif animal == " cat":
+    print("you have enter cat")
+else:   
+    print("worrng input")
+
+
