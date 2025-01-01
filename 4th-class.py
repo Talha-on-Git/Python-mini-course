5ba5bdc4ff1b2f8fb179607521ce46a49ced8fd1
Talha-on-Git <talhayousuf107@gmail.com>
2025-01-01 10:22:08 +0500

    working

--- 4th-class.py
+++ 4th-class.py
@@ -1,84 +1,84 @@
-# x = 0
-# while not(1<= x <=100):
-#     x = int(input("enter  anumber between 1 and 100"))
-# print("vaild",x)
+x = 0
+while not(1<= x <=100):
+    x = int(input("enter  anumber between 1 and 100"))
+print("vaild",x)
 
-# sum = 0 
-# number = int(input("enter a nummber: "))
-# sum = sum + number
+sum = 0 
+number = int(input("enter a nummber: "))
+sum = sum + number
 
-# while number !=0  and sum < 50:
-#     number = int(input("enter a another number: "))
+while number !=0  and sum < 50:
+    number = int(input("enter a another number: "))
 
-# arr =[1,2,3,11,9]
-# sum = 0
-# for i in arr:
-#     sum = sum +1
-# print (sum)
-
-
-# number = [1,2,3,4,5]
-# sum = 0
-
-# for i in number:
-#     sum =+sum
-
-# for i in range(len(number)):
-#     sum = sum + number[i]
-
-# print(sum)
-
-# number = [1,2,3,4,5]
-# sum = 0
-
-# for i in number:
-#     sum = sum+i
-
-# mean = sum/len(number)
-# print(mean)
-
-# text = "unaid"
-
-# for i in text:
-#     print(i)
-
-# text = input("enter any words")
-
-# for i in text:
-#     print(i)
-
-# fur = ["apple","banana","grap"] 
-
-# for i in fur:
-#     print(i)
+arr =[1,2,3,11,9]
+sum = 0
+for i in arr:
+    sum = sum +1
+print (sum)
 
 
-# for i in range(1,11):
-#     if i == 5:
-#         continue
-#     print(i)
+number = [1,2,3,4,5]
+sum = 0
+
+for i in number:
+    sum =+sum
+
+for i in range(len(number)):
+    sum = sum + number[i]
+
+print(sum)
+
+number = [1,2,3,4,5]
+sum = 0
+
+for i in number:
+    sum = sum+i
+
+mean = sum/len(number)
+print(mean)
+
+text = "unaid"
+
+for i in text:
+    print(i)
+
+text = input("enter any words")
+
+for i in text:
+    print(i)
+
+fur = ["apple","banana","grap"] 
+
+for i in fur:
+    print(i)
 
 
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(i)
 
 
 
 
-# fur = ["apple", "banana", "grap"]
 
-# # Outer loop for each fruit
-# for i in fur:
-#     print("Fruit",i)
+
+fur = ["apple", "banana", "grap"]
+
+# Outer loop for each fruit
+for i in fur:
+    print("Fruit",i)
     
-#     # Inner loop for each character of the fruit
-#     for char in i:
-#         print(f"  Character: ",char)
+    # Inner loop for each character of the fruit
+    for char in i:
+        print(f"  Character: ",char)
 
-# list1 = []
+list1 = []
 
-# for i in range(5):
-#     add = int(input("enter a number: "))
-#     list1.append(add)
-#     print(list1)
+for i in range(5):
+    add = int(input("enter a number: "))
+    list1.append(add)
+    print(list1)
 
 
 user = int(input("range katni honi chahiy: "))
